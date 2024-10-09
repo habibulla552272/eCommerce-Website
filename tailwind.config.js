edf68b3b61ee51fdd6dfaf7a11c9bb3ea357bafe
habibulla552272 <mdhabibulla146425@gmail.com>
2024-10-10 04:48:53 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        shop_bg: "url('Image/Rectangle 1.png')",
+      },
+    },
+  },
+  plugins: [],
+};
